c45e1d0ba7010abf2ea53bbff89741679a5806e6
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-11-21 15:58:28 -0800

    Add user creation and login

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,15 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
