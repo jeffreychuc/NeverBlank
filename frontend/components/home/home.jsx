984149335b0e545e533729d4020d41a6c0e25ce8
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-11-22 00:42:30 -0800

    Switch landing page to bootstrap-react

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Jumbotron, Col, Navbar, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 class Home extends React.Component{
@@ -27,18 +28,30 @@ class Home extends React.Component{
   render()  {
     return(
       <div>
-        <div className='flexWrapper'>
-          <div className='top'>
-            <img className='logo' src='assets/Evernote-logo.svg'></img>
-            <a className='login-link' href='/#/signin'>Login</a>
-          </div>
+        <Grid>
+          <Navbar>
+            <Col md={10}>
+            <Navbar.Header>
+              <img className='logo' src='assets/Evernote-logo.svg'></img>
+            </Navbar.Header>
+            </Col>
+            <Col md={2}>
+              <NavItem href='#/login'> Login </NavItem>
+            </Col>
+          </Navbar>
           <div className='splash-row'>
-            <div className='splashText'>
-              <h2>Welcome to NeverBlank</h2>
-            </div>
-            <div className="divider-wrapper">
-              <div className="divider"></div>
-            </div>
+            <Col md={6}>
+              <Jumbotron>
+                <h1>Welcome to NeverBlank.</h1>
+                <p>Capture, organize, and share notes from anywhere. Your best ideas are always with you and always in sync.</p>
+              </Jumbotron>
+            </Col>
+            <Col md={1}>
+              <div className="divider-wrapper">
+                <div className="divider"></div>
+              </div>
+            </Col>
+            <Col md={5}>
             <div className = "signup-form">
               <h2>Sign Up</h2>
                 <form className = "signup-form-element">
@@ -47,8 +60,9 @@ class Home extends React.Component{
                   <button className='signup-element signup-button' onClick ={this.handleSubmit}>Sign up for free</button>
                 </form>
             </div>
+            </Col>
           </div>
-        </div>
+        </Grid>
       </div>
     );
   }
