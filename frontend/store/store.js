356355b9e1fe9d9ac8787492928f5ba065d19af3
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-11-24 11:06:15 -0800

    fix root_reducer reference

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
 
-import rootReducer from '../reducers/root';
+import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
 export default (preloadedState = {}) => createStore(
